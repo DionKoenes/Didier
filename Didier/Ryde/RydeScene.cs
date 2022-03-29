35a4ec4b9447b0b4a600b4bd27d76f80791d97ce
DionKoenes <koenesdion@gmail.com>
2022-03-29 10:15:38 +0200

    Remove obj/bin

--- Didier/Ryde/RydeScene.cs
+++ Didier/Ryde/RydeScene.cs
@@ -15,19 +15,57 @@ namespace Didier
 
 		public RydeScene() : base()
 		{
+			Reload();
+		}
+
+		public void Reload()
+		{
+			Children.Clear();
+			State = State.Playing;
+
+			background = new SpriteNode("resources/achtergrond.png");
+			background.Position = new Vector2((int)Settings.ScreenSize.X / 2, (int)Settings.ScreenSize.Y / 2);
+			AddChild(background);
+
 			bike = new SpriteNode("resources/rydecar.png");
 			bike.Position = new Vector2((int)Settings.ScreenSize.X / 2, (int)Settings.ScreenSize.Y / 2);
 			AddChild(bike);
-
-			background = new SpriteNode("resources/achtergond.png");
-			background.Position = new Vector2((int)Settings.ScreenSize.X / 2, (int)Settings.ScreenSize.Y / 2);
-			AddChild(background);
 		}
 
-
-		public override void Update(float deltaTime)
+		private void HandleInput(float deltaTime)
 		{
-
+			// Reload Game
+			if (Raylib.IsKeyReleased(KeyboardKey.KEY_R))
+			{
+				Reload();
+				Console.WriteLine("Reloaded!!!");
+			}
+			// Player Rotate
+			if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+			{
+				bike.RotateRight(deltaTime);
+			}
+			if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+			{
+				bike.RotateLeft(deltaTime);
+			}
+			if (Raylib.IsKeyReleased(KeyboardKey.KEY_RIGHT))
+			{
+				bike.StopRotating();
+			}
+			if (Raylib.IsKeyReleased(KeyboardKey.KEY_LEFT))
+			{
+				bike.StopRotating();
+			}
+			// Player Thrust
+			if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+			{
+				bike.Thrust();
+			}
+			if (Raylib.IsKeyReleased(KeyboardKey.KEY_UP))
+			{
+				bike.NoThrust();
+			}
 		}
 	}
 }
